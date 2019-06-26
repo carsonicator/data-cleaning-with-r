559d7d8776fc5aa41016138e1a52a996d6e3a1f4
carsonicator <mbcarson@gmail.com>
2019-06-26 14:02:40 -0500

    added .r script

--- data_cleaning_and_exploration.r
+++ data_cleaning_and_exploration.r
@@ -136,3 +136,60 @@ companies$Num_widgets[is.na(companies$Num_widgets)] <- median(companies$Num_widg
 # Recode negative (impossible) values in the Num_widgets column to NA
 companies$Num_widgets[companies$Num_widgets < 0] <- NA
 
+# Duplicates
+#
+# Are any of the rows in our data frame duplicated?
+# Returns the array index of the first duplicate if any, otherwise 0.
+anyDuplicated(companies)
+
+    # a	Dufus & Dingus Ltd.		500000	1	yes	1/1/14 10:00
+    # b	Snooty Pants Fashion	silver     	100000	5	  no	1/1/14 13:24
+    # b	Snooty Pants Fashion	silver     	100000	5	  no	1/1/14 13:24
+    # b	Snooty Pants Fashion	silver     	100000	5	  no	1/1/14 13:24
+    # c	Harry Ham Handlers	gold	123409	67	no	1/1/14 15:05
+
+# Logical vector that shows duplicates
+duplicated(companies)
+
+# Row indeces of duplicates
+# https://stackoverflow.com/questions/12495345/find-indices-of-duplicated-rows
+which(duplicated(companies) | duplicated(companies[nrow(companies):1, ],fromLast = TRUE)[nrow(companies):1])
+
+# Works too. Why use the above command?
+which(duplicated(companies))
+
+# Create a new data frame of only unique rows
+unique_companies <- unique(companies)
+
+# Same using the dplyr package
+library(dplyr)
+unique_companies <- companies %>% distinct()
+
+# Remove duplicated rows based on duplication in a specific column
+companies %>% distinct(Account_Name, .keep_all = TRUE)
+
+# For more details you can use a table to find duplicated values in columns that should contain unique
+# values only. Then you can look at the associated full records to see what kind of duplication it is
+# (e.g., full row vs. mistakenly entered Account_Name) 
+
+# Lists the values of 'Account_Name' and the number of times they occur
+occurrences <- data.frame(table(companies$Account_Name))
+
+# tells you which ids occurred more than once.
+occurrences[occurrences$Freq > 1,]
+
+# Returns the records that contain the duplicated 'Account_Name'
+companies[companies$Account_Name %in% occurrences$Var1[occurrences$Freq > 1],]
+
+# Typos
+#
+# Identifiy typos in status categories...
+table(companies$Status, useNA = "ifany")
+
+# then fix them
+companies$Status[companies$Status == "bornze"] <- "bronze"
+
+# Same for the 'Complete' column
+table(companies$Complete, useNA = "ifany")
+companies$Complete[companies$Complete == "yess"] <- "yes"
+companies$Complete[companies$Complete == "NOT"] <- "no"
